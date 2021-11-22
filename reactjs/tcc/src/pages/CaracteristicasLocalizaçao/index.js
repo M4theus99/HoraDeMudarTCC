6c5a673370297c163047fffb6fcef83f73f50494
isabella <isabellavitaldesousa14072005@gmail.com>
2021-11-22 01:04:55 -0300

    okk

--- reactjs/tcc/src/pages/CaracteristicasLocalizaçao/index.js
+++ reactjs/tcc/src/pages/CaracteristicasLocalizaçao/index.js
@@ -8,34 +8,168 @@ export default function Características ()
     return (
 
         <Container>
+           
+            <div class="faixa1">
+                <div class="cabecalho-inicio">
+                    <div class="cabecalho-img">
+                        <Link to="/LoginADM">
+                            <img src="/assets/imagens/logo-claro.png" alt=""/>
+                        </Link>
+                    </div>
 
-            <div class="cabecalho-inicio">
 
-            <div class="cabecalho-img">
-                <Link to="/LoginADM">
-                <img src="/assets/imagens/logo-claro.png" alt=""/>
-                </Link>
+                    <div class="cabecalho-botoes">
+                        <Link to="/Denúncia">Denúncia</Link>
+                        <Link to="/TipoViolencia">Tipos de violência</Link>
+                        <Link to="/TipoAssedio">Tipos de assédio</Link>
+                        <Link to="/MapaDelegacias">Mapa das Delegacia</Link>
+                        <Link to="/AutoEstima">Autoestima</Link>
+                        <Link to="/Chat">Chat</Link>
+                        <Link to="/Cadastro">Cadastro</Link>
+                        <Link to="/Login">Login</Link>                        
+                    </div>
+               </div>
+            
+               <div class="Titulo"> Características </div>
+
+
+               <div class="caracteristicas">
+                   <div class="identificacao">
+                       <div class="nome">
+                           <label> Nome: <input type="text" name=""></input> </label>
+                       </div>
+
+                       <div class="idade">
+                           <label> Idade: <input type="text" name=""></input> </label>
+                       </div>
+                    </div>
+
+                    <div class="formulario1">
+                       <div class="p1">
+                           <div class="pele">
+                               <div class="pele1">Tom da Pele</div>
+                               <label> <input type="radio" name=""></input> Branco </label>
+                               <label> <input type="radio" name=""></input> Pardo </label>
+                               <label> <input type="radio" name=""></input> Amarelo </label>
+                               <label> <input type="radio" name=""></input> Negro </label>
+                               <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+
+                            <div class="olhos">
+                               <div class="olhos1">Cor dos Olhos</div>
+                               <label> <input type="radio" name=""></input> Castanhos </label>
+                               <label> <input type="radio" name=""></input> Azuis </label>
+                               <label> <input type="radio" name=""></input> Verdes </label>
+                               <label> <input type="radio" name=""></input> Pretos </label>
+                               <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+
+                            <div class="cabelo">
+                               <div class="cabelo1">Cabelo</div>
+                               <label> <input type="radio" name=""></input> Liso </label>
+                               <label> <input type="radio" name=""></input> Ondulado </label>
+                               <label> <input type="radio" name=""></input> Cacheado </label>
+                               <label> <input type="radio" name=""></input> Crespo </label>
+                               <label> <input type="radio" name=""></input> Careca </label>
+                               <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+
+                            <div class="corCabelo">
+                               <div class="corCabelo1">Cor do Cabelo</div>
+                               <label> <input type="radio" name=""></input> Loiro </label>
+                               <label> <input type="radio" name=""></input> Ruivo </label>
+                               <label> <input type="radio" name=""></input> Castanho Escuro </label>
+                               <label> <input type="radio" name=""></input> Castanho Claro </label>
+                               <label> <input type="radio" name=""></input> Preto </label>
+                               <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+                    </div>
+
+                        <div class="p2">
+                            <div class="estatura">
+                                <div class="estatura1">Estatura</div>
+                                <label> <input type="radio" name=""></input> Alto </label>
+                                <label> <input type="radio" name=""></input> Médio </label>
+                                <label> <input type="radio" name=""></input> Baixo </label>
+                                <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+
+                            <div class="fisica">
+                                <div class="fisica1">Estatura Física</div>
+                                <label> <input type="radio" name=""></input> Obeso </label>
+                                <label> <input type="radio" name=""></input> Gordo </label>
+                                <label> <input type="radio" name=""></input> Magro </label>
+                                <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+
+                            <div class="faixaEtaria">
+                                <div class="faixaEtaria1">Faixa Etária</div>
+                                <label> <input type="radio" name=""></input> Adolescente </label>
+                                <label> <input type="radio" name=""></input> Adulto </label>
+                                <label> <input type="radio" name=""></input> Idoso </label> 
+                                <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+
+                            <div class="acessorios">
+                                <div class="acessorios1">Acessórios</div>
+                                <label> <input type="radio" name=""></input> Boné </label>
+                                <label> <input type="radio" name=""></input> Chapéu </label>
+                                <label> <input type="radio" name=""></input> Óculos </label>
+                                <label> <input type="radio" name=""></input> Brinco </label>
+                                <label> <input type="radio" name=""></input> Corrente </label>
+                                <label> Outro: <input type="text" name=""></input> </label>
+                            </div>
+                        </div>
+                    
+                </div>
+            </div>
             </div>
 
+            <div class="faixa2">
+                <div class="localizacao"> Localização </div>
 
-            <div class="cabecalho-botoes">
+                <div class="formulario2">
+                <div class="estado">
+                    <label> Estado: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="cidade">
+                    <label> Cidade: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="bairro">
+                    <label> Bairro: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="rua">
+                    <label> Rua: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="numero">
+                    <label> Número: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="cep">
+                    <label> Cep: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="complemento">
+                    <label> Complemento: <input type="text" name=""></input> </label>
+                </div>
+
+                <div class="referencia">
+                    <label> Ponto de Referencia: <input type="text" name=""></input> </label>
+                </div>
 
                 
-                <Link to="/Denuncie">Denúncie</Link>
-                <Link to="/TipoViolencia">Tipos de violência</Link>
-                <Link to="/TipoAssedio">Tipos de assédio</Link>
-                <Link to="/MapaDelegacias">Mapa das Delegacia</Link>
-                <Link to="/AutoEstima">Autoestima</Link>
-                <Link to="/Chat">Chat</Link>
-                <Link to="/Cadastro">Cadastro</Link>
-                <Link to="/Login">Login</Link>
-
-                                              
             </div>
 
+            <div class="botao">
+                <button>Enviar</button>
             </div>
-
             
+            </div>
+
         </Container>
         
     )
--- reactjs/tcc/src/pages/CaracteristicasLocalizaçao/styled.js
+++ reactjs/tcc/src/pages/CaracteristicasLocalizaçao/styled.js
@@ -5,9 +5,316 @@ const Container = styled.div`
   display: column;
   flex-direction: column;
 
-  background: #6CA88B;
-  height:100vh;
+ ;
+  
+  background: #E9E9E9;
 
+  .cabecalho-inicio{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;  
+    background: #8FB482;
+  }
+
+ .cabecalho-img{
+    margin-top: 1em;
+    margin-left: 2em;
+  }
+
+ .cabecalho-botoes a {
+    margin: 0em .4em;
+    
+  }
+
+ .cabecalho-botoes{
+    margin-top: 4em;
+    margin-right: 2em;
+    color: white;
+    
+  }
+
+.logo {
+    width: 150px;
+    height: 90px;
+    margin-right: 30em;
+    margin-top: -6em;
+  }
+
+.Titulo {
+  background: #8FB482;
+  font-family: Roboto-Medium;
+  font-size: 2.2em;
+  color: #FFFFFF;
+  text-align: center;
+  padding: 0.5em;
+}
+
+.identificacao {
+  margin-left: 2.2em;
+  margin-top: 2em;
+}
+
+.nome {
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.idade {
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.faixa1 {
+  height:100vh;
+}
+
+.faixa2 {
+  height:100vh;
+}
+
+.formulario1 {
+  background: #C4C4C4;
+  margin: 2em;
+  border-radius: 25px;
+  padding: 1em;
+}
+
+.p1 {
+  display: flex;
+  flex-direction: row;
+}
+
+.pele {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  margin: 1em;
+  padding: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+.pele1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.olhos {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+.olhos1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.cabelo {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+.cabelo1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.corCabelo {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+.corCabelo1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.p2 {
+  display: flex;
+  flex-direction: row;
+}
+
+.estatura {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+.estatura1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.fisica {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+.fisica1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.faixaEtaria {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+.faixaEtaria1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.acessorios {
+  display: flex;
+  flex-direction: column;
+  background: #E9E9E9;
+  border-radius: 15px;
+  padding: 1em;
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+.acessorios1 {
+  font-family: Roboto-Medium;
+  font-size: 1.2em;
+  text-align: center;
+  padding: 5px;
+}
+
+.localizacao {
+  background: #8FB482;
+  font-family: Roboto-Medium;
+  font-size: 2.2em;
+  color: #FFFFFF;
+  text-align: center;
+  padding: 0.5em;
+  margin-top: 6em;
+}
+
+.formulario2 {
+  background: #C4C4C4;
+  margin: 2em;
+  border-radius: 25px;
+  padding: 1em;
+}
+
+.estado {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.cidade {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.bairro {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.rua {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.numero {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.cep {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.complemento {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.referencia {
+  margin: 1em;
+  font-family: Roboto-Medium;
+  font-size: 1.4em;
+}
+
+.botao {
+  margin-left: 70em;
+
+}
+
+button {
+  background: #012869;
+  color: #FFFFFF;
+  border-radius: 30px;
+  width: 80px;
+  height: 30px;
+  font-family: Roboto-Medium;
+  font-size: 1em;
+}
+
+input {
+  border-radius: 10px;
+  border-color: transparent;
+}
 
 `;
 
